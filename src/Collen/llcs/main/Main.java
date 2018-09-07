7495ba24b273188a75f1da790cd0008f3f676ddd
butlerwillkillu <42875221+butlerwillkillu@users.noreply.github.com>
2018-09-07 14:59:41 -0400

    code

---
+++ src/Collen/llcs/main/Main.java
@@ -0,0 +1,22 @@
+package Collen.llcs.main;
+
+public class Main
+	{
+
+		
+		
+		public static void main(String[] args)
+			{
+				
+				int numberOne = 2;
+				int numberTwo = 2;
+				int answer;
+				
+				answer = numberOne + numberTwo;
+				
+				
+				System.out.println("My answer is " + answer);
+
+			}
+
+	}
